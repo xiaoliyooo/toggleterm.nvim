fcc83e39fc252ce3e6fd05a3edf09434326cdaa8
xiaoliyo <2585960098@qq.com>
2025-08-24 13:33:41 +0800

    fix: content shift after open the terminal

--- lua/toggleterm/terminal.lua
+++ lua/toggleterm/terminal.lua
@@ -502,6 +502,7 @@ function Terminal:open(size, direction)
   ui.hl_term(self)
   -- NOTE: it is important that this function is called at this point. i.e. the buffer has been correctly assigned
   if self.on_open then self:on_open() end
+  vim.api.nvim_win_call(self.window, function() vim.cmd("normal! gg0") end)
 end
 
 ---Open if closed and close if opened
